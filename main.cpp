cf3e2008fe75952731220460ee5fa271c21ddbdf
Emory Freitas <emory.freitas@itriad.org.br>
2017-11-23 10:38:15 -0400

    Init project

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]) 
+{
+    if (argc == 1) {
+        cout << "Usage: " << argv[0] << "<image>" << endl;
+        exit(0);
+    }
+
+    Mat img = imread(argv[1]);
+    Mat imgBW;
+    cvtColor(img, imgBW, CV_BGR2GRAY);
+    adaptiveThreshold(imgBW, imgBW, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 51, 0);
+
+    imshow("image", img);
+    waitKey(0);
+
+    return 0;
+}
